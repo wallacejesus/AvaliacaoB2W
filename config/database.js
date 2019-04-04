b031a4b71069872643eae7a1a2d9f54a55ef2c63
wallace jesus <wallace@topdown.com.br>
2019-04-04 16:27:00 -0300

    ...

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
 const  mongoose = require('mongoose');
-module.exports  =  mongoose.connect('mongodb+srv://user:password@host',{
+module.exports  =  mongoose.connect('mongodb+srv://user:password@host/starwars?retryWrites=true',{
   useNewUrlParser: true
 });
